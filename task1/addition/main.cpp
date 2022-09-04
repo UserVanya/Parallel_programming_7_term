660e2ac57e416be94d769aef4c74a29995c31fd0
UserVanya <general@startsev-ivan.ru>
2022-09-04 09:35:02 +0300

    task 1: done

--- task1/addition/main.cpp
+++ task1/addition/main.cpp
@@ -4,10 +4,13 @@
 int
 main ()
 {
-#pragma omp parallel
+  int n = omp_get_max_threads() - 1;
+#pragma omp parallel shared(n)
   {
+    while(n != omp_get_thread_num()) {}
     std::cout << "Hello, world! Thread num:" << omp_get_thread_num ()
               << " Threads total:" << omp_get_num_threads () << std::endl;
+    n--;
   }
   return 0;
 }

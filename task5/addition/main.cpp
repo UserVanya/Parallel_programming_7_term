f881d30502514ae1473101cc7d0eeb37a5e8891a
UserVanya <general@startsev-ivan.ru>
2022-09-22 10:41:57 +0300

    task5: change difficulty

--- task5/addition/main.cpp
+++ task5/addition/main.cpp
@@ -2,31 +2,45 @@
 #include <cassert>
 #include <iostream>
 #include <cmath>
-
-long double GetElement(size_t pos)
+long double Factorial(int n)
 {
-  if (pos == 0)
-    return 0;
-  else
-    return pos % 2 == 0  ? -1.0 / pos : 1.0 / pos;
+  return (n <= 1) ? 1 : Factorial(n - 1) * n;
 }
+long double GetExpX(size_t pos, long double x)
+{
+  return std::pow(x, pos) / Factorial(pos);
+}
+long double GetCosX(size_t pos, long double x)
+{
+  return (pos % 2 ? -1 : 1) * pow(x, 2 * pos) / Factorial(2* pos); 
+}
+
 int main(int argc, char* argv[])
 {
   assert(!argv[2]);
   assert(argv[1]);
   size_t n = std::atoi(argv[1]);
-  long double sum = 0;
+  long double sum1 = 0, sum2 = 0;
+  double x;
   struct timespec begin, end;
   clock_gettime (CLOCK_REALTIME, &begin);
-#pragma parallel for schedule(dynamic) reduction(+, sum)
+  std::cout << "Enter x:" << std::endl;
+  std::cin >> x;
+
+#pragma parallel for schedule(dynamic) reduction(+, sum1)
   for (size_t i = 0; i < n; i++)
-    sum += GetElement(i);
+    sum1 += GetExpX(i, x);
+
+#pragma parallel for schedule(dynamic) reduction(+, sum2)
+  for (size_t i = 0; i < n; i++)
+    sum2 += GetCosX(i, x * x);
+
   clock_gettime (CLOCK_REALTIME, &end);
   double elapsed = end.tv_sec - begin.tv_sec;
   elapsed
       += (end.tv_nsec - begin.tv_nsec)
              / 1'000'000'000.0;
   std::cout << "Elapsed time: " << elapsed << std::endl;
-  std::cout << "ln(2) = " << sum << std::endl;
+  std::cout << "e^(" << x << ") * cos("<< x * x << ") = " << sum1 * sum2 << std::endl;
   return 0;
 }

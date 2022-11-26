eef69c4cb267c6ddcc14506590cca6465bc8e19a
UserVanya <general@startsev-ivan.ru>
2022-11-27 01:19:54 +0300

    task10:done

--- task10/default/compile_and_run.sh
+++ task10/default/compile_and_run.sh
@@ -1,2 +1,7 @@
 g++ -std=c++17 -fopenmp auxiliary.cpp linear-solver.cpp band-matrix.cpp non-linear-solver.cpp main.cpp -o main
-OMP_NUM_THREADS=$1 ./main
+OMP_NUM_THREADS=4 
+for b in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.46 1.47 1.48 1.49 1.50 1.51 1.52 1.53 1.54
+do
+OMP_NUM_THREADS=4 
+./main $b
+done
--- task10/default/main.cpp
+++ task10/default/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <cassert>
 #include "linear-solver.h"
 #include "band-matrix.h"
 #include "non-linear-solver.h"
 #include "auxiliary.h"
-int main()
+int main(int argc, char *argv[])
 {
-  NonLinearSolver s(0.0, 1.0, 1.0, 1.0, [](long double x, long double y){ return exp(y);}, [](long double x, long double y){ return exp(y);}, 3);
-  std::cout << s.Solve(1e-20).second << std::endl;
+  assert (!argv[2]);
+  assert (argv[1]);
+  double b = atof(argv[1]);
+  NonLinearSolver s(0.0, 1.0, 1.0, b, [](long double x, long double y){ return exp(-y);}, [](long double x, long double y){ return -exp(-y);}, 11);
+  std::stringstream ss;
+  ss << "b=" << b << ".txt";
+  std::ofstream out(ss.str());
+  
+  auto res = s.Solve(1e-10);
+  out << "x" << "\t" << "y" << std::endl;
+  for (size_t i = 0; i < res.first.size(); i++)
+  {
+    out << res.first.at(i) << "\t" << res.second.at(i) << std::endl;
+  }
 }

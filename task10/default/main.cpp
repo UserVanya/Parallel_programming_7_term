5919eff18b3f53d3030b9490ddb1da91b6906e51
UserVanya <general@startsev-ivan.ru>
2022-11-27 00:28:51 +0300

    task10: + parallel reduction

--- task10/default/main.cpp
+++ task10/default/main.cpp
@@ -5,6 +5,6 @@
 #include "auxiliary.h"
 int main()
 {
-  NonLinearSolver s(0.0, 1.0, 1.0, 1.0, [](long double x, long double y){ return exp(-y);}, [](long double x, long double y){ return -exp(-y);}, 3);
-  std::cout << s.Solve(1e-20).second;
+  NonLinearSolver s(0.0, 1.0, 1.0, 1.0, [](long double x, long double y){ return exp(y);}, [](long double x, long double y){ return exp(y);}, 3);
+  std::cout << s.Solve(1e-20).second << std::endl;
 }
